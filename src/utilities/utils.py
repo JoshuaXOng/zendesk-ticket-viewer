54ef326e8ce047279f89e434141bf3bdab507e11
JoshuaXOng <joshuaong2000@gmail.com>
2021-05-06 11:44:01 +1000

    Added resizeString() to Utils. Improved documentation in Utils.py

--- src/utilities/utils.py
+++ src/utilities/utils.py
@@ -1,11 +1,27 @@
 
 class Utils():
+    """ Holds useful methods. """
 
     @staticmethod
     def limitStringLength(string: str) -> str:
+        """ Truncates the string if longer than a certain length. """
         max_length = 30
         if len(string) > max_length:
             string = string[:max_length]
             string += "..."
         return string
 
+    @staticmethod
+    def resizeString(string: str) -> str:
+        """ Resizes the string if longer/shorter than a certain length.
+        If string is shorter, additional whitespaces will be added to increase length.
+        If string is logner, it will get truncated to match a certain length.
+        """
+        desired_length = 30
+        if len(string) > desired_length:
+            string = string[:desired_length-2]
+            string += "..."
+        else: 
+            no_whitespaces = desired_length - len(string)
+            string += " "*no_whitespaces
+        return string
