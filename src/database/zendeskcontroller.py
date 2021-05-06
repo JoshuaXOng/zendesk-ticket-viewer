65a3a8ba8a108616b04a88daab4ecfc945bee665
JoshuaXOng <joshuaong2000@gmail.com>
2021-05-06 14:25:22 +1000

    Added fetchPageOfTickets() to ZendeskController

--- src/database/zendeskcontroller.py
+++ src/database/zendeskcontroller.py
@@ -8,6 +8,30 @@ class ZendeskController():
         self.username = "joshuaong2000@gmail.com/token"
         self.password = "4gPQcPElcbFL02aqvbvTcxSzKm62oUZRuUuKPHmr"
 
+    def fetchPageOfTickets(self, page_no) -> ([Ticket], bool):
+        """ Returns the tickets on a specific page and checks whether there is a next page """
+        
+        tickets_url = "https://joshuaxong.zendesk.com/api/v2/tickets.json?per_page=25&page="+str(page_no)
+        try:
+            response = requests.get(tickets_url, auth=(self.username, self.password))
+            response.raise_for_status()
+        except requests.exceptions.HTTPError:
+            raise
+        except requests.exceptions.RequestException:
+            raise
+
+        response_json = response.json()
+        tickets = []
+        for entry in response_json["tickets"]:
+            tickets.append(Ticket(entry))
+
+        if response_json["next_page"]:
+            has_next_page = True
+        else:
+            has_next_page = False
+
+        return tickets, has_next_page
+
     def fetchAllTickets(self) -> [Ticket]:
         """ Returns the tickets from sending a get request to Zendesk API's tickets endpoint.
         :raises HTTPError: if the request encounters a 404, 401, etc. type error
@@ -15,7 +39,7 @@ class ZendeskController():
         :return: a list of tickets representing the JSON data of the response
         """
 
-        tickets_url = "https://joshuaxong.zendesk.com/api/v2/tickets.json"
+        tickets_url = "https://joshuaxong.zendesk.com/api/v2/tickets.json?"
         try:
             response = requests.get(tickets_url, auth=(self.username, self.password))
             response.raise_for_status()
