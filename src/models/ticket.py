376c26be4d3e96a3dc5dd267c7d74faf7c8ae091
JoshuaXOng <joshuaong2000@gmail.com>
2021-05-06 10:26:45 +1000

    Added constructor to Ticket.

---
+++ src/models/ticket.py
@@ -0,0 +1,16 @@
+
+class Ticket():
+    """ Represents a Zendesk ticket.
+    """
+
+    def __init__(self, data: dict):
+        try:
+            self.id = data["id"] 
+            self.status = data["status"]
+            self.subject = data["subject"]
+            self.created_at = data["created_at"]
+            self.requester_id = data["requester_id"]
+            self.description = data["description"]
+        except KeyError as e:
+            #e.message... raise 
+            raise KeyError("Ticket could not get decoded properly - perhaps data is malformed.")
