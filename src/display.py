e2e97766cb9701709430af62f0be20dd7353477b
JoshuaXOng <joshuaong2000@gmail.com>
2021-05-06 13:19:03 +1000

    Updated documentation in display.py

--- src/display.py
+++ src/display.py
@@ -4,11 +4,13 @@ from utilities.utils import Utils
 import requests
 
 class Display():
+    """ Contains the visuals and logic in which the user is to interact with the system. """
 
     def __init__(self):
         self.zendeskController = ZendeskController()
     
     def menu(self):
+        """ Runs the user interface of the ticket viewer application. """
 
         view_all_tickets = 1
         view_ticket_of_id = 2
@@ -36,6 +38,7 @@ class Display():
                 self._exec_view_ticket_of_id_option()
     
     def _promptOptionSelection(self, options: [int]) -> int:
+        """ Prompts the user to select on of the supplied options. """
         isValid = False
         while not isValid:
             try:
@@ -48,6 +51,7 @@ class Display():
         return command
 
     def _exec_view_all_tickets_option(self):
+        """ The logic for the view all tickets option. """
         try:
             tickets = self.zendeskController.fetchAllTickets()
         except requests.exceptions.HTTPError as e:
@@ -63,6 +67,7 @@ class Display():
                 if not show_next: break
 
     def _exec_view_ticket_of_id_option(self):
+        """ The logic for the view ticket of if option. """
         id_ = Utils.inputValidInt("Select thou id: ")   
         try:
             ticket = self.zendeskController.fetchTicketWithID(id_)
@@ -73,6 +78,7 @@ class Display():
         else: 
             print("\n"+ticket.indepth())
 
-    def _printTicketsOverview(self, tickets: [Ticket]):    
+    def _printTicketsOverview(self, tickets: [Ticket]):
+        """ Prints the supplied list of tickets in a decent manner. """
         for ticket in tickets:
             print(ticket.overview())
