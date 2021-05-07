7c01e77af9ac4ef361d54625a018df211dd90eb2
JoshuaXOng <joshuaong2000@gmail.com>
2021-05-08 02:38:56 +1000

    Updated typing and comments in ZendeskController

--- src/database/zendeskcontroller.py
+++ src/database/zendeskcontroller.py
@@ -8,9 +8,14 @@ class ZendeskController():
         self.username = "joshuaong2000@gmail.com/token"
         self.password = "4gPQcPElcbFL02aqvbvTcxSzKm62oUZRuUuKPHmr"
 
-    def fetchPageOfTickets(self, page_no) -> ([Ticket], bool):
-        """ Returns the tickets on a specific page and checks whether there is a next page """
-        
+    def fetchPageOfTickets(self, page_no: int) -> tuple[list[Ticket], bool, bool]:
+        """ Returns the tickets on a specific page and checks whether there is a next page. 
+        :param page_no: the page number 1-indexed of the page of tickets.
+        :raises HTTPError: if a http error is encountered in the request, i.e. 401, 404, etc.
+        :raises RequestException: a catch all exception for a request.
+        :return: a tuple that contains the list of tickets, two booleans determining if there are previous/next pages.
+        """
+
         tickets_url = "https://joshuaxong.zendesk.com/api/v2/tickets.json?per_page=25&page="+str(page_no)
         try:
             response = requests.get(tickets_url, auth=(self.username, self.password))
@@ -25,15 +30,13 @@ class ZendeskController():
         for entry in response_json["tickets"]:
             tickets.append(Ticket(entry))
 
-        if response_json["next_page"]:
-            has_next_page = True
-        else:
-            has_next_page = False
-
-        return tickets, has_next_page
+        return tickets, response_json["previous_page"], response_json["next_page"]
 
     def fetchTicketCount(self) -> int:
-        """ Returns the number of tickets in the account. """
+        """ Returns the number of tickets in the account. 
+        :raises HTTPError: if a http error is encountered in the request, i.e. 401, 404, etc.
+        :raises RequestException: a catch all exception for a request.
+        """
 
         tickets_url = "https://joshuaxong.zendesk.com/api/v2/tickets/count.json?"
         try:
@@ -48,11 +51,11 @@ class ZendeskController():
         count = response_json["count"]["value"]
         return count
 
-    def fetchAllTickets(self) -> [Ticket]:
+    def fetchAllTickets(self) -> list[Ticket]:
         """ Returns the tickets from sending a get request to Zendesk API's tickets endpoint.
-        :raises HTTPError: if the request encounters a 404, 401, etc. type error
-        :raises RequestException: a catch all for request exceptions
-        :return: a list of tickets representing the JSON data of the response
+        :raises HTTPError: if the request encounters a 404, 401, etc. type error.
+        :raises RequestException: a catch all for request exceptions.
+        :return: a list of tickets representing the JSON data of the response.
         """
 
         tickets_url = "https://joshuaxong.zendesk.com/api/v2/tickets.json?"
@@ -74,9 +77,10 @@ class ZendeskController():
     def fetchTicketWithID(self, id_: int) -> Ticket:
         """ Returns the ticket from sending a get request to Zendesk API's tickets endpoint.
         This request will only fetch a ticket of specific id.
-        :raises HTTPError: if the request encounters a 404, 401, etc. type error
-        :raises RequestException: a catch all for request exceptions
-        :return: a ticket representing the JSON data of the response
+        :param id_: the id of the target ticket.
+        :raises HTTPError: if the request encounters a 404, 401, etc. type error.
+        :raises RequestException: a catch all for request exceptions.
+        :return: a ticket representing the JSON data of the response.
         """
         
         ticket_url = "https://joshuaxong.zendesk.com/api/v2/tickets/"+str(id_)+".json"
