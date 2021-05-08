653579a83c61b1191532aa4507784fa9b2d90caa
JoshuaXOng <joshuaong2000@gmail.com>
2021-05-08 11:06:46 +1000

    Split testutils.py, into: - testinpututils.py and teststringutils.py

---
+++ tests/testinpututils.py
@@ -0,0 +1,50 @@
+import sys, os
+sys.path.insert(0,os.path.abspath('.\src'))
+import unittest
+from unittest import main, TestCase
+from unittest.mock import patch
+from utilities.inpututils import InputUtils
+
+class TestInputUtils(TestCase):
+    """ A group of unit tests for the methods contained within InputUtils. """
+
+    def test_inputOption(self):
+        
+        # Test that inputOption() rejects invalid options 2 and -1, then accepts 4.
+        mock_input = ["2","-1","4"]
+        with patch('builtins.input', side_effect=mock_input):
+            dummy_options = [0,4,10]
+            dummy_text = "A prompt message and an error message."
+            result = InputUtils.inputOption(dummy_options, dummy_text, dummy_text)
+            self.assertEqual(4,result)
+        
+        # Test that inputOption() works first try.
+        mock_input = ["2","3","4"]
+        with patch('builtins.input', side_effect=mock_input):
+            dummy_options = [2,4,10]
+            dummy_text = "A prompt message and an error message."
+            result = InputUtils.inputOption(dummy_options, dummy_text, dummy_text)
+            self.assertEqual(2,result)
+
+    def test_inputValidInt(self):
+
+        # Test that inputValidInt() rejects a letter and accepts 7.
+        mock_input = ["y","7"]
+        with patch('builtins.input', side_effect=mock_input):
+            result = InputUtils.inputValidInt("Mocking an input: ")
+            self.assertEqual(7,result)
+
+        # Test that inputValidInt() rejects a double and accepts an integer.
+        mock_input = ["4.4","9"]
+        with patch('builtins.input', side_effect=mock_input):
+            result = InputUtils.inputValidInt("Mocking an input: ")
+            self.assertEqual(9,result)
+
+    def test_inputOneOrZero(self):
+        mock_input = ["0.0","2","1"]
+        with patch('builtins.input', side_effect=mock_input):
+            result = InputUtils.inputOneOrZero("Mocking an input: ")
+            self.assertEqual(1,result)
+
+if __name__ == "__main__":
+    unittest.main(buffer=True)
--- tests/teststringutils.py
+++ tests/teststringutils.py
@@ -3,27 +3,15 @@ sys.path.insert(0,os.path.abspath('.\src'))
 import unittest
 from unittest import main, TestCase
 from unittest.mock import patch
-from utilities.utils import Utils
+from utilities.stringutils import StringUtils
 
-class TestUtils(TestCase):
+class TestStringUtils(TestCase):
     """ A group of unit tests for the methods contained within Utils. """
 
     def test_resizeString(self):
         empty_space = 20*" "
         self.assertEqual("0123456789"+empty_space, Utils.resizeString("0123456789"))
         self.assertEqual("012345678901234567890123456...", Utils.resizeString("0123456789012345678901234567890123456789"))
-
-    def test_inputValidInt(self):
-        mock_input = ["y","7"]
-        with patch('builtins.input', side_effect=mock_input):
-            result = Utils.inputValidInt("Mocking an input: ")
-            self.assertEqual(7,result)
-
-    def test_inputOneOrZero(self):
-        mock_input = ["2","1"]
-        with patch('builtins.input', side_effect=mock_input):
-            result = Utils.inputOneOrZero("Mocking an input: ")
-            self.assertEqual(1,result)
 
 if __name__ == "__main__":
     unittest.main(buffer=True)
