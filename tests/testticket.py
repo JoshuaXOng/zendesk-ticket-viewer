c6d8b584225bfa72be26b15f24d34f1f0435a070
JoshuaXOng <joshuaong2000@gmail.com>
2021-05-08 11:19:40 +1000

    Added more tests to TestTicket

--- tests/testticket.py
+++ tests/testticket.py
@@ -2,7 +2,7 @@ import sys, os
 sys.path.insert(0,os.path.abspath('.\src'))
 import unittest
 from models.ticket import Ticket
-from utilities.utils import Utils
+from utilities.stringutils import StringUtils
 
 class TestTicket(unittest.TestCase):
     """ A group of unit tests for the methods contained within Ticket. """
@@ -18,6 +18,16 @@ class TestTicket(unittest.TestCase):
     }
     test_ticket = Ticket(test_json)
 
+    test_json_2 = {
+        "id": 34,
+        "status": "open",
+        "subject": "Please test me 2",
+        "created_at": "2077-05-20T07:15:00Z",
+        "requester_id": "23854720435",
+        "description": "Test me"
+    }
+    test_ticket_2 = Ticket(test_json_2)
+
     test_json_malformed = {
         "id": 106,
         "created_at": "2000-10-31T04:34:05Z",
@@ -26,20 +36,26 @@ class TestTicket(unittest.TestCase):
     }
 
     def test_overview(self):
+        # This test assumes StringUtils.resizeString() works correctly.
+
         output = self.test_ticket.overview()
-        resized_subject = Utils.resizeString(self.test_ticket.subject)
-        self.assertEqual(output, 
-            "|| ID: 106, Status: open || Subject: "+resized_subject+" || Requested at: 31 Oct 2000 04:34:05"
-        )
+        resized_subject = StringUtils.resizeString(self.test_ticket.subject, 30) 
+        self.assertEqual(output, "|| ID: 106, Status: open || Subject: "+resized_subject+" || Requested at: 31 Oct 2000 04:34:05")
+
+        output = self.test_ticket_2.overview()
+        resized_subject = StringUtils.resizeString(self.test_ticket_2.subject, 30) 
+        self.assertEqual(output, "|| ID: 34, Status: open || Subject: "+resized_subject+" || Requested at: 20 May 2077 07:15:00")
 
     def test_indepth(self):
+        
         output = self.test_ticket.indepth()
-        self.assertEqual(output, 
-            "|| ID: 106, Status: open || Subject: Please test me || Requested at: 31 Oct 2000 04:34:05 || Requested by: 1337\n\nCan thou give me a test"
-        )
+        self.assertEqual(output, "|| ID: 106, Status: open || Subject: Please test me || Requested at: 31 Oct 2000 04:34:05 || Requested by: 1337\n\nCan thou give me a test")
+
+        output = self.test_ticket_2.indepth()
+        self.assertEqual(output, "|| ID: 34, Status: open || Subject: Please test me 2 || Requested at: 20 May 2077 07:15:00 || Requested by: 23854720435\n\nTest me")
 
     def test_constructor(self):
-        self.assertRaises(KeyError,Ticket,self.test_json_malformed)
+        self.assertRaises(KeyError, Ticket, self.test_json_malformed)
 
 if __name__ == "__main__":
     unittest.main()
