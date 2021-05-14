3b3db8d6ebdcfc1184d011129eb96017852d1037
JoshuaXOng <joshuaong2000@gmail.com>
2021-05-14 23:36:20 +1000

    Added extra test to TestStringUtils

--- tests/teststringutils.py
+++ tests/teststringutils.py
@@ -1,5 +1,5 @@
 import sys, os
-sys.path.insert(0,os.path.abspath('.\src'))
+sys.path.insert(0, os.path.abspath('.\src'))
 import unittest
 from unittest import main, TestCase
 from unittest.mock import patch
@@ -12,13 +12,16 @@ class TestStringUtils(TestCase):
         
         # Test short string gets sized up.
         empty_space = 20*" "
-        self.assertEqual("0123456789"+empty_space, StringUtils.resizeString("0123456789",30))
+        self.assertEqual("0123456789"+empty_space, StringUtils.resizeString("0123456789", 30))
 
         # Test perfect length string does not change.
-        self.assertEqual("012345678901234567890123456789", StringUtils.resizeString("012345678901234567890123456789",30))
+        self.assertEqual("012345678901234567890123456789", StringUtils.resizeString("012345678901234567890123456789", 30))
 
         # Test long string gets sized down.
-        self.assertEqual("012345678901234567890123456...", StringUtils.resizeString("0123456789012345678901234567890123456789",30))
+        self.assertEqual("012345678901234567890123456...", StringUtils.resizeString("0123456789012345678901234567890123456789", 30))
+        
+        # Test long string gets resized correctly to supplied length.
+        self.assertEqual(30, len(StringUtils.resizeString("0123456789012345678901234567890123456789", 30)))
 
 if __name__ == "__main__":
     unittest.main(buffer=True)
